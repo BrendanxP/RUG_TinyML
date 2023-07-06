20cf1e571aece1336a6d5fa7a8a3dae2a2057ce0
BrendanxP <brendan.j.dijkstra@gmail.com>
2023-07-06 15:59:40 +0200

    Update CameraCaptureRawBytes.ino
    Partly dynamic code for changing resolution

--- extras/CameraCaptureRawBytes/CameraCaptureRawBytes.ino
+++ extras/CameraCaptureRawBytes/CameraCaptureRawBytes.ino
@@ -2,34 +2,63 @@
   OV767X - Camera Capture Raw Bytes
 
   This sketch reads a frame from the OmniVision OV7675 camera
-  and writes the bytes to the Serial port. Use the Procesing
+  and writes the bytes to the Serial port. Use the Processing
   sketch in the extras folder to visualize the camera output.
 
   This example code is in the public domain.
 */
 
 #include <TinyMLShield.h>
+#include "OV767X_TinyMLx.h"
 
 int bytesPerFrame;
 
-byte data[176 * 144 * 2]; // QCIF: 176x144 X 2 bytes per pixel (RGB565)
+// Define the desired resolution
+// STILL NEED TO CHANGE 1 PIECE BELOW AS WELL
+String resolution = "QCIF"; // Choose from VGA, CIF, QVGA, QCIF, or QQVGA
 
 void setup() {
+  int cameraWidth, cameraHeight;
+  if (resolution.equals("VGA")) {
+    cameraWidth = 640;
+    cameraHeight = 480;
+  } else if (resolution.equals("CIF")) {
+    cameraWidth = 352;
+    cameraHeight = 240;
+  } else if (resolution.equals("QVGA")) {
+    cameraWidth = 320;
+    cameraHeight = 240;
+  } else if (resolution.equals("QCIF")) {
+    cameraWidth = 176;
+    cameraHeight = 144;
+  } else if (resolution.equals("QQVGA")) {
+    cameraWidth = 160;
+    cameraHeight = 120;
+  } else {
+    // Default to QVGA if an invalid resolution is provided
+    cameraWidth = 320;
+    cameraHeight = 240;
+  }
+
+  byte data[cameraWidth * cameraHeight * 2]; // Default to QVGA: 320x240 X 2 bytes per pixel (RGB565)
+
   Serial.begin(9600);
   while (!Serial);
 
+  //THIS ALSO NEEDS TO BE CHANGED
   if (!Camera.begin(QCIF, RGB565, 1, OV7675)) {
     Serial.println("Failed to initialize camera!");
     while (1);
   }
 
-  bytesPerFrame = Camera.width() * Camera.height() * Camera.bytesPerPixel();
+  bytesPerFrame = cameraWidth * cameraHeight * Camera.bytesPerPixel();
 
   // Optionally, enable the test pattern for testing
   // Camera.testPattern();
 }
 
 void loop() {
+  byte data[bytesPerFrame];
   Camera.readFrame(data);
 
   Serial.write(data, bytesPerFrame);
