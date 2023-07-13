99bbecd91850146c163d4e2849b22cb590566dff
BrendanxP <brendan.j.dijkstra@gmail.com>
2023-07-13 12:13:07 +0200

    Update hello_world_lite.ino

--- examples/hello_world_lite/hello_world_lite.ino
+++ examples/hello_world_lite/hello_world_lite.ino
@@ -1,4 +1,4 @@
-/**
+/*
  * Test sinewave neural network model
  * 
  * Author: Pete Warden
@@ -21,7 +21,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
+==============================================================================*/
 
 // Import TensorFlow
 #include "TensorFlowLite.h"
